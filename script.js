5d806dc10407ecb9f07fc28910b8787d03ad4a14
rajpranz <rajeshwarrprasad@yahoo.com>
2022-06-08 11:45:19 +1200

    Add gameboard object

--- script.js
+++ script.js
@@ -8,6 +8,26 @@ const player = name => {
     return{getName, incrementScore, getScore};
 }
 
-const player1 = player("Player 1");
-const player2 = player("Player 2");
+const Player1 = player("Player 1");
+const Player2 = player("Player 2");
 
+const GameBoard = (() => {
+    const board = [0,0,0,0,0,0,0,0,0];
+    const getBoard = () => board;
+    const addMove = (position, move) =>{
+        if(board[position] == 0){
+            board.splice(position, 1, move);
+            return true;
+        }
+        else
+            return false;
+
+    }
+    const getState = () => {
+        if((board[0]==board[1]==board[2]) || (board[0]==board[1]==board[2]) || (board[0]==board[1]==board[2])) return `${board[0]} wins`;
+        if(board[0]==board[1]==board[2]) return `${board[0]} wins`;
+        if(board[0]==board[1]==board[2]) return `${board[0]} wins`;
+
+    }
+    return {getBoard, addMove}
+})();

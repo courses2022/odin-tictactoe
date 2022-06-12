8cf5e43149272c6eed955c689558dc916c384391
rajpranz <rajeshwarrprasad@yahoo.com>
2022-06-13 11:31:38 +1200

    Update game status on user input

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ const player = (name, move) => {
     const getName = () =>  name;
     const getMove = () => move;
     const resetScore = () => score = 0;
-    return{getName, incrementScore, getScore, resetScore};
+    return{getName, incrementScore, getScore, resetScore, getMove};
 }
 
 
 const GameBoard = (() => {
-    const board = [0,0,0,0,0,0,0,0,0];
+    let board = ["","","","","","","","",""];
     const getBoard = () => board;
     const addMove = (position, move) =>{
         if(board[position] == 0){
@@ -24,17 +24,17 @@ const GameBoard = (() => {
 
     };
     const getState = () => {
-        if(((board[0]==board[1] && board[1]==board[2]) || (board[0]==board[3]&&board[3]==board[6]) || (board[0]==board[4]&&board[4]==board[8])) && board[0]!= 0) return `${board[0]} wins`;
-        if((board[3]==board[4] && board[4]==board[5]) && board[3]!= 0) return `${board[3]} wins`;
-        if((board[6]==board[7] && board[7]==board[8]) && board[6]!= 0) return `${board[6]} wins`;
-        if((board[1]==board[4] && board[4]==board[7]) && board[1]!= 0) return `${board[1]} wins`;
-        if((board[2]==board[5] && board[5]==board[8]) && board[2]!= 0) return `${board[2]} wins`;
-        if(board.includes(0)) return "active";
-        if(!board.includes(0)) return "draw";
+        if(((board[0]==board[1] && board[1]==board[2]) || (board[0]==board[3]&&board[3]==board[6]) || (board[0]==board[4]&&board[4]==board[8])) && board[0]!= "") return `${board[0]} wins`;
+        if((board[3]==board[4] && board[4]==board[5]) && board[3]!= "") return `${board[3]} wins`;
+        if((board[6]==board[7] && board[7]==board[8]) && board[6]!= "") return `${board[6]} wins`;
+        if((board[1]==board[4] && board[4]==board[7]) && board[1]!= "") return `${board[1]} wins`;
+        if((board[2]==board[5] && board[5]==board[8]) && board[2]!= "") return `${board[2]} wins`;
+        if(board.includes("")) return "active";
+        if(!board.includes("")) return "draw";
 
 
     };
-    const resetBoard = () => {board = [0,0,0,0,0,0,0,0,0]}
+    const resetBoard = () => {board = ["","","","","","","","",""]}
     return {getBoard, addMove, getState, resetBoard}
 })();
 
@@ -48,7 +48,7 @@ const DisplayController = ((selector = ".board") =>{
             const newContent = document.createTextNode(`${x}`);
             newDiv.appendChild(newContent);
             newDiv.setAttribute("onclick", `GameController.playMove(${index})`);
-            if(x!= " ")newDiv.classList.add("full");
+            if(x!= "")newDiv.classList.add("full");
             root.appendChild(newDiv);
         });
         
@@ -60,6 +60,7 @@ const DisplayController = ((selector = ".board") =>{
 
 const GameController = (() => {
     const notification = document.querySelector(".notification");
+    const newRound = document.querySelector(".newRound");
     
     const Player1 = player("Player 1", "X");
     const Player2 = player("Player 2", "O");
@@ -69,8 +70,9 @@ const GameController = (() => {
     const initialiseGame = () => {
         
         currentPlayer = Player1;
-        DisplayController.drawBoard();
-        
+        GameBoard.resetBoard();
+        DisplayController.drawBoard(GameBoard.getBoard());
+        newRound.style.display = "none";
     }
 
     const swapPlayers = () => {
@@ -89,6 +91,23 @@ const GameController = (() => {
 
     const playMove = (i) => {
         console.log(`${i} was clicked`);
+        if(GameBoard.getState() == "active"){
+            GameBoard.addMove(i, currentPlayer.getMove())
+            DisplayController.drawBoard(GameBoard.getBoard());
+            
+        }
+
+        if(GameBoard.getState() != "active"){
+            if(GameBoard.getState() == "draw"){
+                notification.innerHTML = `It's a draw`;    
+            }
+            else{
+                notification.innerHTML = `${currentPlayer.getName()} wins`;
+            }
+            newRound.style.display = "inline";
+        }else{
+            swapPlayers();
+        }
 
     }
     return {initialiseGame, newGame, playMove};
